59e39699bc78d63e4c5d8d13bce42ec18d1bb074
Tony Blard <tonyblard55@gmail.com>
2020-01-28 15:47:04 +0100

    create cookie in header

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -45,5 +46,8 @@ class SecurityController extends AbstractController
       return new JsonResponse([
         'email' => $user->getEmail()
       ]);
+
+      $response->headers->setCookie(Cookie::create('foo', 'bar'));
+      return $response;
     }
 }
